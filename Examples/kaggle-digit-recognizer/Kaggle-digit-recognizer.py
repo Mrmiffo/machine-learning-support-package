5d61cb4cac0363061ff9f229e0cca5da29b36fae
Anton Strandman <Marvin.Ztrubas@gmail.com>
2018-10-30 16:18:51 +0100

    Even more comments on the digit recognizer!

--- Examples/kaggle-digit-recognizer/Kaggle-digit-recognizer.py
+++ Examples/kaggle-digit-recognizer/Kaggle-digit-recognizer.py
@@ -4,6 +4,11 @@ from keras.utils import to_categorical
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Input paremeters
+image_width = 28
+image_height = 28
+channels = 1
+classes = 10
 
 # Set hyperparameters
 epochs = 20
@@ -12,24 +17,24 @@ batch_size = 256
 
 # Load the training data
 
-train_data = np.genfromtxt('Examples/kaggle-digit-recognizer/train.csv', delimiter=',') # There are quicker ways to load data, but this is simple :)
+train_data = np.genfromtxt('Examples/kaggle-digit-recognizer/train.csv', delimiter=',')     # There are quicker ways to load data, but this is simple :)
 
-train_data = np.delete(train_data, 0, axis = 0)                 # Delete the header row
-train_y = to_categorical(train_data[:,0], 10)                   # Create a onehot encoding of the possible outputs. For example the value 4 becomes [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
-train_x = np.delete(train_data, 0, axis = 1)                    # Remove the first column containing the lable
-train_x = np.reshape(train_x, (train_x.shape[0], 28, 28, 1))    # Reshape the 1D vector image to a 2D vector with 1 channel (greyscale). Shape is now (#ofSamples, width, heigt, #ofChannels)
+train_data = np.delete(train_data, 0, axis = 0)                                             # Delete the header row
+train_y = to_categorical(train_data[:,0], classes)                                          # Create a onehot encoding of the possible outputs. For example the value 4 becomes [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+train_x = np.delete(train_data, 0, axis = 1)                                                # Remove the first column containing the lable
+train_x = np.reshape(train_x, (train_x.shape[0], image_width, image_height, channels))      # Reshape the 1D vector image to a 2D vector with 1 channel (greyscale). Shape is now (#ofSamples, width, heigt, #ofChannels)
 print("Found %d training samples" % (train_x.shape[0]))
 
 # Load the test data
 
 test_x = np.genfromtxt('Examples/kaggle-digit-recognizer/test.csv', delimiter=',') 
-test_x = np.delete(test_x, 0, axis = 0)                        # Delete the header row
-test_x = np.reshape(test_x, (test_x.shape[0], 28, 28, 1))      # Reshape the 1D vector image to a 2D vector.
+test_x = np.delete(test_x, 0, axis = 0)                                                     # Delete the header row
+test_x = np.reshape(test_x, (test_x.shape[0], image_width, image_height, channels))         # Reshape the 1D vector image to a 2D vector.
 print("Found %d test samples" % (test_x.shape[0]))
 
 # Create the model
 model = Sequential([
-    Conv2D(16, 3, padding='same', input_shape=(28,28,1), activation='relu'), 
+    Conv2D(16, 3, padding='same', input_shape=(image_width,image_height,channels), activation='relu'), 
     Conv2D(16, 3, padding='same', activation='relu'), 
     MaxPooling2D(),
     Conv2D(16, 3, padding='same', activation='relu'), 
@@ -42,12 +47,13 @@ model = Sequential([
 ])
 
 # Train the model
-model.compile('adam', loss='categorical_crossentropy', metrics=['accuracy'])    # Adam is generally a reliable optimizer. As this is a classification task, categorical_crossentropy is used as the loss. Accuracy is a good metric for a classification task.
-model.fit(train_x, train_y, batch_size=batch_size, validation_split=validation_split, epochs=epochs) # Train the network with the given hyperparameters (see top)
+model.compile('adam', loss='categorical_crossentropy', metrics=['accuracy'])                            # Adam is generally a reliable optimizer. As this is a classification task, categorical_crossentropy is used as the loss. Accuracy is a good metric for a classification task.
+model.fit(train_x, train_y, batch_size=batch_size, validation_split=validation_split, epochs=epochs)    # Train the network with the given hyperparameters (see top)
 
-pred = model.predict(test_x) # Predict
-pred_to_print = np.reshape(test_x, (test_x.shape[0], 28, 28)) # imshow does not want the channel value for greyscale images, so rescale it to shape (28,28) instead of (28, 28, 1)
+# Predict results
+pred = model.predict(test_x)                                                        # Use the model to predict the output of all the test samples (test samples has no "true" label so we don't know what they are)
+pred_to_print = np.reshape(test_x, (test_x.shape[0], image_width, image_height))    # imshow does not want the channel value for greyscale images, so rescale it to shape (28,28) instead of (28, 28, 1)
 for i in range(20):
-    print("Prediction: %d" %(np.argmax(pred[i])))
-    plt.imshow(pred_to_print[i],cmap='gray')
+    print("Prediction: %d" %(np.argmax(pred[i])))   # This is what our network predicted for this image
+    plt.imshow(pred_to_print[i],cmap='gray')        # Display the image so we can see what it looks like
     plt.show()
