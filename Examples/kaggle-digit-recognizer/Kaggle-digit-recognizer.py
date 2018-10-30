c39c3d2eef7695e6d730ad6582b1a72e18779f97
Anton Strandman <Marvin.Ztrubas@gmail.com>
2018-10-30 15:55:42 +0100

    Added environment.yml, updated readme and added the digit recognizer example

---
+++ Examples/kaggle-digit-recognizer/Kaggle-digit-recognizer.py
@@ -0,0 +1,54 @@
+from keras.layers import Dense, Dropout, Conv2D, Flatten, MaxPooling2D
+from keras.models import Sequential
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils import to_categorical
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Set hyperparameters
+epochs = 20
+validation_split = 0.2
+batch_size = 256
+
+# Load the training data
+
+train_data = np.genfromtxt('Examples/kaggle-digit-recognizer/train.csv', delimiter=',') # There are quicker ways to load data, but this is simple :)
+
+train_data = np.delete(train_data, 0, axis = 0)                 # Delete the header row
+train_y = to_categorical(train_data[:,0], 10)                   # Create a onehot encoding of the possible outputs. For example the value 4 becomes [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+train_x = np.delete(train_data, 0, axis = 1)                    # Remove the first column containing the lable
+train_x = np.reshape(train_x, (train_x.shape[0], 28, 28, 1))    # Reshape the 1D vector image to a 2D vector with 1 channel (greyscale). Shape is now (#ofSamples, width, heigt, #ofChannels)
+print("Found %d training samples" % (train_x.shape[0]))
+
+# Load the test data
+
+test_x = np.genfromtxt('Examples/kaggle-digit-recognizer/test.csv', delimiter=',') 
+test_x = np.delete(test_x, 0, axis = 0)                        # Delete the header row
+test_x = np.reshape(test_x, (test_x.shape[0], 28, 28, 1))      # Reshape the 1D vector image to a 2D vector.
+print("Found %d test samples" % (test_x.shape[0]))
+
+# Create the model
+model = Sequential([
+    Conv2D(16, 3, padding='same', input_shape=(28,28,1), activation='relu'), 
+    Conv2D(16, 3, padding='same', activation='relu'), 
+    MaxPooling2D(),
+    Conv2D(16, 3, padding='same', activation='relu'), 
+    Conv2D(16, 3, padding='same', activation='relu'), 
+    MaxPooling2D(),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dense(64, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+# Train the model
+model.compile('adam', loss='categorical_crossentropy', metrics=['accuracy'])    # Adam is generally a reliable optimizer. As this is a classification task, categorical_crossentropy is used as the loss. Accuracy is a good metric for a classification task.
+model.fit(train_x, train_y, batch_size=batch_size, validation_split=validation_split, epochs=epochs) # Train the network with the given hyperparameters (see top)
+
+pred = model.predict(test_x) # Predict
+pred_to_print = np.reshape(test_x, (test_x.shape[0], 28, 28)) # imshow does not want the channel value for greyscale images, so rescale it to shape (28,28) instead of (28, 28, 1)
+for i in range(20):
+    print("Prediction: %d" %(np.argmax(pred[i])))
+    plt.imshow(pred_to_print[i],cmap='gray')
+    plt.show()
--- README.md
+++ README.md
@@ -1,7 +1,32 @@
-## Welcome to the machine learning support package
-The purpose of this project is to provide a convenient set of tool to speed up development of machine learning algorithms.
+# Welcome to the machine learning support package
+The purpose of this project is to provide a convenient set of tools to speed up development of machine learning algorithms.
 
-# Setup
+## Setup
+This project runs on python 3.6 using Keras with Tensorflow backend. The guide below is created for windows users, Linux/Mac users might have different commands.
+
 1. Install Anaconda: https://www.anaconda.com/download/
 2. Open the conda prompt.
-3. From the root of this folder give the command "" to install all the required packages.
+3. From the root of this folder write "conda env create -f environment.yml" to install all the required packages.
+    For more information regarding conda package managment please see: https://conda.io/docs/user-guide/tasks/manage-environments.html
+4. Write "conda activate mlsp" to activate the newly created environment.
+    For Linux/Mac use "source activate mlsp"
+
+
+### Optional
+The environment automatically installs the tensorflow-gpu package. This allows for Keras to automatically utilize any compatible GPU on the system. 
+Please follow the following guidelines from TensorFlow for how to install CUDA drivers for compatible nVIDIA cards: 
+https://www.tensorflow.org/install/gpu
+
+NOTE: 
+- CUDA Toolkit 10 does NOT work with tensorflow. Please follow the guidelines strictly to avoid issues.
+- Your "normal" graphics drivers may become outdated by installing CUDA. Reinstalling the latest graphics drivers after CUDA install is recommended and should not affect tensorflow.
+
+## Notes on machine learning
+Here are some things to keep in mind when working with machine learning in python.
+### NumPy
+Machine learning algorithms use linear algebra computations in order to parallelize the computations. and NumPy introduce new N-dimensional array objects (implemented in C code in the backend) with extended linera algerbra functionality.
+Whenever you'r working with arrays or matricies, use numpy 
+
+### For-loops
+Just don't if it can be avoided. For-loops in python are very slow and if used in code that run frequently it may grind your code to a standstill. For-loops are still perfectly fine to use for infrequent code pieces (such as iterating over different hyperparameters). 
+
